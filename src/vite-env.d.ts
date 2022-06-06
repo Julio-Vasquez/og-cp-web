8d18c0ed1d31c05da17904758a7541e4462cb800
Julio-Vasquez <jualvalitube@gmail.com>
2022-06-05 20:44:36 -0500

    first commit

---
+++ src/vite-env.d.ts
@@ -0,0 +1,12 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_NAME: string
+  readonly VITE_API_KEY: string
+  readonly VITE_TOKEN: string
+  readonly VITE_BASE_URL: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
