ddaad23bd6b293edcfe053b7b4cf3f324958eb47
Julio-Vasquez <jualvalitube@gmail.com>
2022-10-17 22:16:14 -0500

    feat(env): init

--- src/common/constants/environment.constant.ts
+++ src/common/constants/environment.constant.ts
@@ -1,7 +1,19 @@
-export const JWT_KEY = '&%@#**/IPA-PC-GO-KEY/**#@%&'
+export const JWT_KEY = import.meta.env.VITE_JWT_KEY
 
-export const MAX_TIME_FETCH = 28000 // milisecons
+export const MAX_TIME_FETCH = import.meta.env.VITE_MAX_TIME_FETCH // milisecons
 
-export const URL_API = 'http://127.0.0.1:8550/api'
+export const URL_API = import.meta.env.VITE_BASE_URL
 
-export const IS_DEV = true
+export const APP_NAME = import.meta.env.VITE_APP_NAME
+
+export const MODES = {
+    dev: 'dev',
+    prod: 'production',
+    staging: 'staging',
+}
+
+export const IS_DEV = import.meta.env.VITE_MODE === MODES.dev
+
+export const IS_PROD = import.meta.env.VITE_MODE === MODES.prod
+
+export const IS_STAGING = import.meta.env.VITE_MODE === MODES.staging
