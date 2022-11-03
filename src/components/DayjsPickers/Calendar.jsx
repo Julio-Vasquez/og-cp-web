ab99feb47646b518e31d580561b467a891eb1bdd
Julio-Vasquez <jualvalitube@gmail.com>
2022-11-02 20:01:10 -0500

    feat(datepicker): remove moment to dayjs

--- src/components/DayjsPickers/Calendar.jsx
+++
@@ -1,5 +0,0 @@
-import configGenerateDayJs from 'rc-picker/lib/generate/dayjs'
-import generateCalendar from 'antd/es/calendar/generateCalendar'
-import 'antd/es/calendar/style'
-
-export const Calendar = generateCalendar(configGenerateDayJs)
