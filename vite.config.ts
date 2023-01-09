117a30f962b591fbf69b463bbacf11ad0413a97b
Julio-Vasquez <jualvalitube@gmail.com>
2023-01-08 20:38:50 -0500

    feat(vite): port

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd(), '')
     return {
+        define: {
+            PORT: env.VITE_PORT,
+        },
         plugins: [react()],
         server: {
             port: env.VITE_PORT,
