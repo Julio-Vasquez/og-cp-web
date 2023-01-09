8a9ffdb0cf94664d68f85ebdd5a212114b988f9d
Julio-Vasquez <jualvalitube@gmail.com>
2023-01-08 20:50:05 -0500

    feat(vite.config): removed not required configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,7 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, process.cwd(), '')
-    return {
-        define: {
-            PORT: env.VITE_PORT,
-        },
-        plugins: [react()],
-        server: {
-            port: env.VITE_PORT,
-        },
-    }
+export default defineConfig({
+    plugins: [react()],
 })
