b307d29e7ce43796c20b5f9b790c229214e6cecf
CarlosPerez93 <Carlos.Perez3@outlook.es>
2023-10-26 01:25:26 +0200

    refactor(layout): relized styles

--- src/components/LayoutPrivate/LayoutPrivate.tsx
+++ src/components/LayoutPrivate/LayoutPrivate.tsx
@@ -1,32 +1,31 @@
-import { Layout } from 'antd'
+import { FC, useState } from 'react'
+import { Layout, theme } from 'antd'
+
+import SiderP from './SiderP'
+import NavBar from './NavBar'
 import {
     LayoutPrivateProps,
     LayoutPrivateDefaultProps,
     LayoutPrivatePropsTypes,
 } from './layoutPrivate.types'
-import { FC, lazy, useState } from 'react'
 
 import './Layout.scss'
-import SiderP from './SiderP'
-import NavBar from './NavBar'
 
 const { Content } = Layout
 
 export const LayoutPrivate: FC<LayoutPrivateProps> = ({ children }) => {
     const [collapsed, setCollapsed] = useState(false)
-
+    const {
+        token: { colorBgContainer },
+    } = theme.useToken()
     return (
         <Layout className='main-layout'>
             <SiderP collapsed={collapsed} />
             <Layout>
                 <NavBar collapsed={collapsed} setCollapsed={setCollapsed} />
                 <Content
-                    style={{
-                        margin: '24px 16px',
-                        padding: 24,
-                        minHeight: 280,
-                        background: 'gray',
-                    }}
+                    className='main-layout__content'
+                    style={{ background: colorBgContainer }}
                 >
                     {children}
                 </Content>
