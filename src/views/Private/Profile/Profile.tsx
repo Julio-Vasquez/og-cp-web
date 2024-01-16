6776f8587b3867567ebc630e71a5331f34db275a
CarlosPerez93 <carlos.perez3@outlook.es>
2024-01-16 22:54:03 +0100

    refactor(styles): applicated grid style

--- src/views/Private/Profile/Profile.tsx
+++ src/views/Private/Profile/Profile.tsx
@@ -1,4 +1,4 @@
-import { Divider, Space, Spin } from 'antd'
+import { Divider, Spin } from 'antd'
 
 import { Modals } from '../../../components/Modal'
 import iconUser from '../../../assets/svg/iconUser.svg'
@@ -15,58 +15,51 @@ import './Profile.scss'
 export const Profile = () => {
     const { visible, openDialog, closeDialog } = useVisible()
 
-    const { data: userMe, loading: loadingUserMe } = useGet<ResponseFetch<DataUser>>(
-        { functionFetch: api.defaultData.userMe }
-    )
+    const { data: userMe, loading } = useGet<ResponseFetch<DataUser>>({
+        functionFetch: api.defaultData.userMe,
+    })
 
     const { formatMessage } = useIntl()
 
     const payload = userMe.status === 'success' ? userMe.payload : undefined
 
     return (
-        <Spin spinning={loadingUserMe}>
+        <Spin spinning={loading}>
             <div className='main-profile'>
-                <div className='main-profile__container-photo'>
-                    <img
-                        src={iconUser}
-                        alt='photo profile'
-                        className='main-profile__photo'
-                    />
-                </div>
+                <img
+                    src={iconUser}
+                    alt='photo profile'
+                    className='main-profile__photo'
+                />
                 <Divider />
                 <div className='main-profile__data'>
-                    <Space direction='vertical'>
-                        <h1 className='main-profile__data__name'>
-                            {payload?.fullName ??
-                                formatMessage({ id: 'text.fullName' })}
-                        </h1>
-                        <h3 className='main-profile__data__items'>
-                            {formatMessage({ id: 'text.username' })}
-                        </h3>
-                        <p>{payload?.username}</p>
-                        <h3 className='main-profile__data__items'>
-                            {formatMessage({ id: 'text.mail' })}
-                        </h3>
-                        <p>{payload?.mail}</p>
-                        <h3 className='main-profile__data__items'>
-                            {formatMessage({ id: 'text.birthDate' })}
-                        </h3>
-                        <p>{payload?.birthDate}</p>
-                    </Space>
-                    <Space direction='vertical'>
-                        <h3 className='main-profile__data__items'>
-                            {formatMessage({ id: 'text.role' })}
-                        </h3>
-                        <p>{payload?.role}</p>
-                    </Space>
+                    <h1 className='main-profile__data__name'>
+                        {payload?.fullName ?? formatMessage({ id: 'text.fullName' })}
+                    </h1>
+                    <div className='main-profile__data__user'>
+                        <p>{formatMessage({ id: 'text.username' })}</p>
+                        <span>{payload?.username}</span>
+                    </div>
+                    <div className='main-profile__data__mail'>
+                        <p>{formatMessage({ id: 'text.mail' })}</p>
+                        <span>{payload?.mail}</span>
+                    </div>
+                    <div className='main-profile__data__birth-date'>
+                        <p>{formatMessage({ id: 'text.birthDate' })}</p>
+                        <span>{payload?.birthDate}</span>
+                    </div>
+                    <div className='main-profile__data__role'>
+                        <p>{formatMessage({ id: 'text.role' })}</p>
+                        <span>{payload?.role}</span>
+                    </div>
                 </div>
-                <Modals
-                    isOpen={visible}
-                    openModal={openDialog}
-                    closeModal={closeDialog}
-                    children={<h1>hi!</h1>}
-                />
             </div>
+            <Modals
+                isOpen={visible}
+                openModal={openDialog}
+                closeModal={closeDialog}
+                children={<h1>hi!</h1>}
+            />
         </Spin>
     )
 }
