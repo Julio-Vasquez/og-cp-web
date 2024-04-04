d0ce552d3c6b23b5b6c941e9be4855b6f299f0e2
CarlosPerez93 <carlos.perez3@outlook.es>
2024-04-05 00:57:47 +0200

    refactor(Profile):  component design completed

---
+++ src/views/Private/EditProfile/EditProfile.tsx
@@ -0,0 +1,5 @@
+import PersonalInformation from '../../../components/Steps/PersonalInformation'
+
+export const EditProfile = () => {
+    return <></>
+}
--- src/views/Private/Profile/Profile.tsx
+++ src/views/Private/Profile/Profile.tsx
@@ -1,29 +1,67 @@
-import { Spin } from 'antd'
+import { Form, Spin } from 'antd'
 
 import { CardPresentation } from '../../../components/CardPresentation/CardPresentation'
 
 import api from '../../../api'
-import useIntl from '../../../hooks/useIntl'
 import { useQuery } from '../../../hooks/api'
 
 import { Status } from '../../../utils/constants/status.enum'
 import { DataUser } from '../../../utils/types/userData.type'
 
 import './Profile.scss'
+import PersonalInformation from '../../../components/Steps/PersonalInformation'
+import CustomButton from '../../../components/Buttons/CustomButton'
+import { useState } from 'react'
 
 export const Profile = () => {
-    const { formatMessage } = useIntl()
+    const [active, setActive] = useState<boolean>(false)
     const { data: userMe, loading } = useQuery<DataUser>({
         functionFetch: api.user.userMe,
     })
 
     const payload =
         userMe.status === Status.success ? userMe.payload : ({} as DataUser)
+    const handleSetState = () => setActive(!active)
+    const onFinish = (values: any) => {
+        console.log(values)
+    }
 
     return (
         <Spin spinning={loading}>
             <div className='main-profile'>
+                <div className='main-profile__btn-active-edit'>
+                    <CustomButton
+                        width='300px'
+                        children='Edit'
+                        onClick={handleSetState}
+                        disable={!active}
+                    />
+                </div>
                 <CardPresentation payload={payload} />
+                <div className='main-profile__container-form'>
+                    <Form
+                        className='main-profile__form-edit-profile'
+                        disabled={active}
+                        onFinish={onFinish}
+                        autoComplete='off'
+                        layout='vertical'
+                    >
+                        <PersonalInformation loading={loading} />
+                        <div className='main-profile__btn-container'>
+                            <CustomButton
+                                width='200px'
+                                disable={active}
+                                onClick={handleSetState}
+                                children='cancel'
+                            />
+                            <CustomButton
+                                width='200px'
+                                htmlType='submit'
+                                children='Edit'
+                            />
+                        </div>
+                    </Form>
+                </div>
             </div>
         </Spin>
     )
