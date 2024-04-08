5d9eb834694d2acb042e399d64e0f97423a338e2
CarlosPerez93 <carlos.perez3@outlook.es>
2024-04-08 22:18:32 +0200

    refactor(CustomAvatar): component relocation

--- src/components/Avatars/CustomAvatar/CustomAvatar.tsx
+++ src/components/Avatars/CustomAvatar/CustomAvatar.tsx
@@ -6,6 +6,7 @@ import { CustomAvatarProps } from './customAvatar.type'
 import p1 from '../../assets/svg/pegatina-1.svg'
 import p2 from '../../assets/svg/pegatina-2.svg'
 import p3 from '../../assets/svg/pegatina-3.svg'
+import p4 from '../../assets/svg/pegatina-4.svg'
 
 export const CustomAvatar: FC<CustomAvatarProps> = ({ percent }) => {
     return (
@@ -15,7 +16,9 @@ export const CustomAvatar: FC<CustomAvatarProps> = ({ percent }) => {
                     ? p3
                     : percent > 30 && percent <= 70
                     ? p2
-                    : p1
+                    : percent > 70 && percent < 100
+                    ? p1
+                    : p4
             }
         />
     )
