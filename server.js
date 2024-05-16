f3f327f714d6e3c0cd1be91c919f181c6efa8c8c
Julio-Vasquez <jualvalitube@gmail.com>
2024-05-15 19:14:29 -0500

    feat(test): railway 2

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path')
 const app = express()
 
 // Sirve los archivos estáticos del directorio 'build'
-app.use(express.static(path.join(__dirname, 'build')))
+app.use(express.static(path.join(__dirname, 'dist')))
 
 // Redirige todas las solicitudes a 'index.html'
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'))
 })
 
 const port = process.env.PORT || 3000
