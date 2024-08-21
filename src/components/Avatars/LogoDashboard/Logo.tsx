0008675c6bb8882fa7c47aab364967c4481bd80b
CarlosPerez93 <carlos.perez3@outlook.es>
2024-08-21 19:19:13 +0200

    refactor(Logo): added link

--- src/components/Avatars/LogoDashboard/Logo.tsx
+++ src/components/Avatars/LogoDashboard/Logo.tsx
@@ -3,14 +3,17 @@ import { LogoProps, LogoPropsDefault, LogoPropsTypes } from './logo.type'
 
 import image from '../../../assets/img/image.png'
 
+import { Link } from 'react-router-dom'
+import { ROUTES_PRIVATE as RP } from '../../../utils/constants/routes.constants'
+
 import './Logo.scss'
 
-export const LogoDashboard: FC<LogoProps> = ({ collapsed }) => (
-    <div className='main-logo'>
+export const LogoDashboard: FC<LogoProps> = () => (
+    <Link to={RP.dashboard} className='main-logo'>
         <div className='main-logo__container-img'>
             <img src={image} alt='img' />
         </div>
-    </div>
+    </Link>
 )
 
 LogoDashboard.propTypes = LogoPropsTypes
